e0a97f2f413fb5eab31ce6d44c8abef80b635527
Mahadi Hasan <mahadih137@gmail.com>
2023-10-06 17:53:17 +0600

    fixed responsive issue

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,8 @@
-
-
 import { Outlet } from "react-router-dom";
 
 function MainLayout() {
   return (
-    <div className="relative max-w-6xl mx-4 md:mx-10 lg:mx-auto">
+    <div className="relative max-w-6xl mx-4 lg:mx-auto">
       <Outlet />
     </div>
   );
